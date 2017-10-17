ffa1807c4d7044a3729c3c33302c6809cbfcef30
DannyRux <dangorlov@yahoo.com>
2017-10-16 22:09:16 -0400

    Fixed firebase issue

--- Cravin'/assets/js/app.js
+++ Cravin'/assets/js/app.js
@@ -1,11 +1,15 @@
 var config = {
-    apiKey: "AIzaSyAfZPBHzrGWnwIrnZTZrw8LBet6cKYPIoM",
-    authDomain: "codersbay-a751d.firebaseapp.com",
-    databaseURL: "https://codersbay-a751d.firebaseio.com",
-    projectId: "codersbay-a751d",
-    storageBucket: "codersbay-a751d.appspot.com",
-    messagingSenderId: "1033281453498"
+        apiKey: "AIzaSyAfZPBHzrGWnwIrnZTZrw8LBet6cKYPIoM",
+        authDomain: "codersbay-a751d.firebaseapp.com",
+        databaseURL: "https://codersbay-a751d.firebaseio.com",
+        projectId: "codersbay-a751d",
+        storageBucket: "codersbay-a751d.appspot.com",
+        messagingSenderId: "1033281453498"
 };
+firebase.initializeApp(config);
+var database = firebase.database();
+
+
 
 function User(uid, name, email, photoURL) {
     this.uid = uid;
@@ -17,6 +21,8 @@ function User(uid, name, email, photoURL) {
     this.currentState = "";
     this.cities = [];
     this.restaurants = [];
+    this.userHasEaten = false;
+    this.lastRestaurantId = "";
 }
 
 function Restaurant(id, name, address, lat, lon, thumb, price_range, average_cost, featured_image, aggregate_rating) {
@@ -40,7 +46,7 @@ function Restaurant(id, name, address, lat, lon, thumb, price_range, average_cos
 
 
 var apiKeys = {
-    mapsEmbed: "AIzaSyAwobEcHv202FHv77LcDWxJCrbDNaxqUoE",
+    mapsEmbed: "AIzaSyDqf6wEb0fLt7Wf56hzb84dYQ8OBte-5dE",
     zomato: {
         header: {
             "user-key": "029229483ea9d14f003cd7257516abde"
@@ -55,11 +61,16 @@ var apiKeys = {
 
 var apiUrls = {
     zomatoBase: "https://developers.zomato.com/api/v2.1/",
-    yelpBestMatch: "GET https://api.yelp.com/v3/businesses/matches/best",
+    yelpBestMatch: "https://api.yelp.com/v3/businesses/matches/best",
+    googleDirsUrl: "https://www.google.com/maps/dir/",
+    googleGeoLocation: `https://www.googleapis.com/geolocation/v1/geolocate?key=${ apiKeys.mapsEmbed}`,
 };
 
-function getDatabaseLocation(uid) {
+function getUsrDataLoc(uid) {
     return "users/" + uid;
+}
+function getRestDataLoc(rid) {
+    return "restaurants/" + rid;    
 }
 
 
@@ -76,8 +87,6 @@ function distance(lat1, lon1, lat2, lon2) {
 }
 
 
-firebase.initializeApp(config);
-var database = firebase.database();
 var provider = new firebase.auth.GoogleAuthProvider();
 
 $(document).ready(function () {
@@ -137,15 +146,12 @@ $(document).ready(function () {
                 app.currentUser.currentCityId = response.location_suggestions[0].id;
                 app.currentUser.currentCity = response.location_suggestions[0].name;
                 app.currentUser.currentState = response.location_suggestions[0].state_code;
-                database.ref(getDatabaseLocation(app.currentUser.uid)).update({
+                database.ref(getUsrDataLoc(app.currentUser.uid)).update({
                     currentCityId: app.currentUser.currentCityId,
                     currentCity: app.currentUser.currentCity,
                     currentState: app.currentUser.currentState,
                 });
             });
-        },
-        getCraving: function () {
-
         },
         showAlert(title, body) {
 
@@ -204,12 +210,14 @@ $(document).ready(function () {
                                     <h4>Average Rating: ${ currentValue.aggregate_rating }</h4>
                                     <h4>Address: ${ currentValue.address }</h4>
                                     <h4>About ${ currentValue.distance } miles away.</h4>
+                                    <h5>
+                                        <a href="${ apiUrls.googleDirsUrl }${app.latLong[0]},${app.latLong[1]}/${currentValue.googleAddress}" class="go-to-restaurant" data-index="${ index}">Let's Go!</a>
+                                    </h5>
                                 </div>
                             </div>
                        </div>
                        
-                    </div>`
-                    );
+                    </div>`);
                 resultsBox.append(resultsDisplay);
             });
         },
@@ -248,6 +256,19 @@ $(document).ready(function () {
             })
 
         },
+        addRestuarantToDb: function(idx, callback) {
+            var restaurant = app.restaurantResults[idx];
+            var dbRestLoc = getRestDataLoc(restaurant.id);
+            var dbUsrLoc = getUsrDataLoc(app.currentUser.uid);
+            
+            database.ref(dbRestLoc).once("value", function(snapshot) { 
+                var restData = snapshot.val();
+                if(!restData) {
+                    database.ref(dbRestLoc).set(restaurant);
+                }
+            });
+            database.ref(dbUsrLoc).update( { userHasEaten: true, lastRestaurantId: restaurant.id });
+        },
         eventListeners: function () {
             $("#btn-sign-out").on("click", function () {
                 firebase.auth().signOut().then(function () {
@@ -257,7 +278,12 @@ $(document).ready(function () {
                     // An error happened.
                 });
             });
-
+            $(document).on("click", ".go-to-restaurant", function (e) {
+                e.preventDefault();
+                app.addRestuarantToDb(parseInt($(this).attr("data-index")),function() { 
+                    window.open($(this).attr("href"));
+                });
+            });
             $("#btn-fast").on('click', function (e) {
                 e.preventDefault();
                 app.findCraving("fast")
@@ -270,17 +296,7 @@ $(document).ready(function () {
             $("#btn-sign-in").on("click", function () {
                 firebase.auth().signInWithRedirect(provider);
             });
-            $("#btn-go").on('click', function () {
-                app.selectedRestaurant = app.restaurantResults[0];
-                callUrl = `${ apiUrls.googlePlacesSearch }&location=${ app.selectedRestaurant.lat },${ app.selectedRestaurant.lon }&rankby=distance&name=${ app.selectedRestaurant.name }`;
-                app.callApi("get", callUrl, {
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "https://danorlovsky.github.io/",
-                    "Access-Control-Allow-Headers": "Content-Type"
-                }, function (response) {
-                    console.log(response);
-                });
-            });
+
             firebase.auth().getRedirectResult().then(function (result) {
                 if (result.credential) {
                     var token = result.credential.accessToken;
@@ -288,12 +304,12 @@ $(document).ready(function () {
                 app.currentUser = result.user;
                 if (result.user != null) {
 
-                    database.ref(getDatabaseLocation(app.currentUser.uid)).once("value", function (snapshot) {
+                    database.ref(getUsrDataLoc(app.currentUser.uid)).once("value", function (snapshot) {
                         var userData = snapshot.val();
                         console.log(userData);
                         if (!userData) {
                             app.currentUser = new User(app.currentUser.uid, app.currentUser.displayName, app.currentUser.email, app.currentUser.photoURL);
-                            database.ref(getDatabaseLocation(app.currentUser.uid)).update(app.currentUser);
+                            database.ref(getUsrDataLoc(app.currentUser.uid)).update(app.currentUser);
                         }
                     })
                 }
@@ -309,15 +325,20 @@ $(document).ready(function () {
             firebase.auth().onAuthStateChanged(function (user) {
 
                 if (user) {
-                    databaseLocation = getDatabaseLocation(user.uid);
+                    databaseLocation = getUsrDataLoc(user.uid);
                     app.currentUser = user;
-                    navigator.geolocation.getCurrentPosition(function (position) {
-                        app.latLong = [position.coords.latitude, position.coords.longitude];
-                        console.log(app.latLong);
-                        app.getCity();
+                    if (navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(function (position) {
+                            app.latLong = [position.coords.latitude, position.coords.longitude];
+                            console.log(app.latLong);
+                            app.getCity();
 
-                    });
-                    console.log("Open cravings!");
+                        });
+                    } else {
+                        app.callApi("get", apiUrls.googleGeoLocation, "", function (response) {
+                            app.latLong = [ response.location.lat, response.location.lng ];
+                        });    
+                    }
                     // GO TO THE NEXT PAGE
                     app.switchScreens("#login-screen", "#craving-select-screen");
                 } else {
@@ -329,6 +350,7 @@ $(document).ready(function () {
         },
 
     }
+    
     setTimeout(function () {
         app.switchScreens("#splash-screen", "#login-screen");
         app.eventListeners();
